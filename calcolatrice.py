a394c7605b984937409ef32b7ad26dbfd44d8899
SamueleBresciani <bresciani.samuele.07@itisdalmine.edu.it>
2024-12-17 09:44:33 +0100

    Update calcolatrice.py

--- calcolatrice.py
+++ calcolatrice.py
@@ -1,20 +1,21 @@
-scelta = -1
-while(scelta!=0):
-    print("Benvenuto nella nostra calcolatrice")
-    print("Inseisci l'operazione che vuoi effettuare")
-    scelta = int(input("1) Sottrazzione\n2) Addizzione\n0) Exit"))
-
-    if scelta == 0:
-        break
-    elif scelta == 1:
-        n_1 = int(input("inserisci il primo numero: "))
-        n_2 = int(input("inserisci il secondo numero: "))
-        risultato = n_1 - n_2
-        print(f"{n_1} - {n_2} =  {risultato}")
-    elif scelta == 2:
-        n_1 = int(input("inserisci il primo numero: "))
-        n_2 = int(input("inserisci il secondo numero: "))
-        risultato = n_1 + n_2
-        print(f"{n_1} + {n_2} =  {risultato}")
-    else:
-        print("Scelta non corretta!")
+def somma():  
+    n_1=int(input("inserisci il primo numero !")) 
+        n_2=int(input("inserisci il secondo numero !")) 
+        risultato=n_1+n_2
+        print(f"{n_1}+{n_2}={risultato}")
+while True:
+    print("benvenuto nella nostra calcolatrice") 
+    print("inserisci l'operazione che vuoi effettuare: ") 
+    scelta =int(input("1)sottrazione\n2)addizione\n") )
+    if scelta ==0 : 
+        break 
+    elif scelta==1: 
+        n_1=int(input("inserisci il primo numero !")) 
+        n_2=int(input("inserisci il secondo numero !")) 
+        risultato=n_1-n_2
+        print(f"{n_1}-{n_2}={risultato}")
+ 
+    elif scelta==2: 
+          somma()  
+    else: 
+        print("scelta non corretta !!")
