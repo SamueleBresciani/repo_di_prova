38378ed6cdd6172f62a178e5444542a0b0c8320f
SamueleBresciani <bresciani.samuele.07@itisdalmine.edu.it>
2024-12-17 09:57:29 +0100

    Update calcolatrice.py

--- calcolatrice.py
+++ calcolatrice.py
@@ -9,15 +9,3 @@ def sottrazzione():
         risultato=n_1-n_2
         print(f"{n_1}-{n_2}={risultato}")
  
-while True:
-    print("benvenuto nella nostra calcolatrice") 
-    print("inserisci l'operazione che vuoi effettuare: ") 
-    scelta =int(input("1)sottrazione\n2)addizione\n") )
-    if scelta ==0 : 
-        break 
-    elif scelta==1: 
-        sottrazzione()
-    elif scelta==2: 
-        somma()  
-    else: 
-        print("scelta non corretta !!")
