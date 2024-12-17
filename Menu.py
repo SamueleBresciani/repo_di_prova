c603285d40f747eb68372e3d59158ce55f674a42
SamueleBresciani <bresciani.samuele.07@itisdalmine.edu.it>
2024-12-17 09:57:38 +0100

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -1,1 +1,12 @@
-print("hello")
+while True:
+    print("benvenuto nella nostra calcolatrice") 
+    print("inserisci l'operazione che vuoi effettuare: ") 
+    scelta =int(input("1)sottrazione\n2)addizione\n") )
+    if scelta ==0 : 
+        break 
+    elif scelta==1: 
+        sottrazzione()
+    elif scelta==2: 
+        somma()  
+    else: 
+        print("scelta non corretta !!")
