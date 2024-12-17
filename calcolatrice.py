84ebe473a9a61f7fdfe2656f29f4286ac21f5993
SamueleBresciani <bresciani.samuele.07@itisdalmine.edu.it>
2024-12-17 09:47:16 +0100

    Update calcolatrice.py

--- calcolatrice.py
+++ calcolatrice.py
@@ -3,6 +3,12 @@ def somma():
         n_2=int(input("inserisci il secondo numero !")) 
         risultato=n_1+n_2
         print(f"{n_1}+{n_2}={risultato}")
+def sottrazzione():
+        n_1=int(input("inserisci il primo numero !")) 
+        n_2=int(input("inserisci il secondo numero !")) 
+        risultato=n_1-n_2
+        print(f"{n_1}-{n_2}={risultato}")
+ 
 while True:
     print("benvenuto nella nostra calcolatrice") 
     print("inserisci l'operazione che vuoi effettuare: ") 
@@ -10,12 +16,8 @@ while True:
     if scelta ==0 : 
         break 
     elif scelta==1: 
-        n_1=int(input("inserisci il primo numero !")) 
-        n_2=int(input("inserisci il secondo numero !")) 
-        risultato=n_1-n_2
-        print(f"{n_1}-{n_2}={risultato}")
- 
+        sottrazzione()
     elif scelta==2: 
-          somma()  
+        somma()  
     else: 
         print("scelta non corretta !!")
